5bd495f86f1aab147f964c5d3545e9c0303f87b4
Manuel Pineyro <manuel.o.pineyro@gmail.com>
2016-10-17 13:18:11 -0400

    init

---
+++ lib/dataingestion.ex
@@ -0,0 +1,3 @@
+defmodule DataIngestion do
+
+end
---
+++ test/dataingestion_test.exs
@@ -0,0 +1,8 @@
+defmodule DataingestionTest do
+  use ExUnit.Case
+  doctest Dataingestion
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
